5aecd35c48b70fd6d6d62d3842d16f856f2247b0
kuldeep506 <hard31259@gmail.com>
2021-04-21 13:21:29 +0530

    delete method done

--- src/BST.java
+++ src/BST.java
@@ -5,33 +5,30 @@ public class BST {
         this.root = null;
     }
 
-    public void insert(int value) {
-        Node node = new Node(value);
-        if (isEmpty()) {
-            root = node;
-        } else {
-            Node temp = root;
-            Node current = null;
-            while (temp != null) {
-                current = temp;
-                if (value <= temp.data) {
-                    temp = temp.left;
-                } else {
-                    temp = temp.right;
-                }
-            }
-            if (value <= current.data) {
-                current.left = node;
-            } else {
-                current.right = node;
-            }
+
+    /**
+     * //method to insert a node in binary search tree.
+     */
+    public void insert(int data) {
+        root = insert(root, data);
+    }
+
+    private Node insert(Node node, int data) {
+        Node newNode = new Node(data);
+        if (node == null) {
+            return newNode;
         }
+        if (data < node.data) {
+            node.left = insert(node.left, data);
+        } else {
+            node.right = insert(node.right, data);
+        }
+        return node;
     }
 
-    public boolean isEmpty() {
-        return root == null;
-    }
-
+    /**
+     * inorder traversal
+     */
     public void inOrderTraversal() {
         inOrderTraversal(root);
     }
@@ -46,11 +43,14 @@ public class BST {
         }
     }
 
+    /**
+     * prOrder traversal
+     */
     public void preOrderTraversal() {
         preOrderTraversal(root);
     }
 
-    private void preOrderTraversal(Node node) {
+    public void preOrderTraversal(Node node) {
         if (node == null) {
             return;
         } else {
@@ -60,6 +60,9 @@ public class BST {
         }
     }
 
+    /**
+     * postOrder traversal
+     */
     public void postOrderTraversal() {
         preOrderTraversal(root);
     }
@@ -74,23 +77,10 @@ public class BST {
         }
     }
 
-    public boolean search(int value) {
-        if (isEmpty()) {
-            System.out.println("empty");
-        }
-        Node temp = root;
-        while (temp != null) {
-            if (value > temp.data) {
-                temp = temp.right;
-            } else if (value < temp.data) {
-                temp = temp.left;
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    /**
+     * method to calculate height of tree
+     * * @return int height
+     */
     public int heightOfTree() {
         return heightOfTree(root);
     }
@@ -104,4 +94,77 @@ public class BST {
         }
         return 1 + Math.max(heightOfTree(node.left), heightOfTree(node.right));
     }
+
+    public Node minNode() {
+        return minNode(root);
+    }
+
+    /**
+     * method to return node having minimum value
+     *
+     * @param root
+     * @return node
+     */
+    private Node minNode(Node root) {
+        if (root.left != null) {
+            return minNode(root.left);
+        } else
+            return root;
+    }
+
+    /**
+     * method to delete a node from binary tree.
+     *
+     * @param value
+     */
+    public void deleteNode(int value) {
+        deleteNode(root, value);
+    }
+
+    private Node deleteNode(Node node, int value) {
+        if (node == null) {
+            return null;
+        }
+
+        //if value is less then given node then search the value in left subtree.
+        if (value < node.data) {
+            node.left = deleteNode(node.left, value);
+
+            //if value is greater then given node then search the value in right subtree.
+        } else if (value > node.data) {
+            node.right = deleteNode(node.right, value);
+
+            //if value is equal to node's data that is we have found node to be deleted.
+        } else {
+
+            //if node to be deleted has no child set node to null.
+            if (node.right == null && node.left == null) {
+                return null;
+
+                //if node to be deleted has single child(right child).
+            } else if (node.left == null) {
+                node = node.right;
+
+                //if node to be deleted has single child(lest child).
+            } else if (node.right == null) {
+                node = node.left;
+
+                // if node is to deleted has two child.
+            } else {
+
+                //find minimum node from right subtree.
+                Node temp = minNode(node.right);
+
+                // assign node to that right subtree's minimum node.
+                node.data = temp.data;
+
+                //delete duplicate node from right subtree.
+                node.right = deleteNode(node.right, value);
+            }
+
+        }
+        return node;
+
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
+import sun.reflect.generics.tree.Tree;
+
 public class Main {
     public static void main(String[] args) {
         BST obj =new BST();
-       obj.insert(5);
-        obj.insert(7);
-        obj.insert(12);
+        obj.insert(6);
+        obj.insert(10);
         obj.insert(2);
-       // obj.inOrderTraversal();
-        System.out.println(obj.search(4));
+        obj.insert(14);
+        obj.insert(1);
+        obj.insert(45);
+        obj.insert(1);
+        obj.insert(0);
+        obj.insert(22);
+        obj.insert(11);
+        obj.deleteNode(45);
+        obj.deleteNode(6);
+
+        obj.preOrderTraversal();
 
     }
 }
